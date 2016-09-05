56c1b40b1628241a7da7b6855e6d1db25b25c801
cer8 <tecumseh25972>
2016-09-05 17:05:23 -0500

    added spaces

---
+++ spaces.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python
+
+string = str(input("String: "))
+multiplier = input("Multiplier:")
+iter_string = range(len(string))
+new_string = ""
+
+if string == "":
+	"Empty string given"
+
+for i in iter_string:
+	new_string += string[i]
+	if i != iter_string[-1]:
+		if string[i] != " ":
+			if string[i+1] != " ":
+				new_string += (" "  * multiplier)
+		else: 
+			new_string += ("  " * multiplier)
+
+print new_string
+
+
+		
